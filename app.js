fee02d0c956e35e4fda7b6b44d79cc4e2ed49e7c
AlexVanSteenhoven <alex.steenhoven@gmail.com>
2019-07-11 23:47:16 +0200

    added modules and added some comments

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const passport = require("passport");
+const path = require("path");
+
+// Initialize Appliation
 const app = express();
 
 // Index Route
@@ -6,6 +13,7 @@ app.get("/", (req, res) => {
   res.json({ msg: "Running Successfully" });
 });
 
+// Listen on the defined port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
